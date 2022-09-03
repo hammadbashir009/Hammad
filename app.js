19e0941b8c25f27b072d3fb308c8b55baf855a2e
Hammad Bashir Junejo <112729503+hammadbashir009@users.noreply.github.com>
2022-09-03 12:51:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+// // Question no 1  
+//  alert ("Well come page") ;
+
+// //Question no 2// 
+// alert ("Error! Please enter a valid password");
+
+// // Question no 3
+// alert ("Well come to JS Land \n Happy Codding");
+
+// //Question no 4//
+//   alert ("Wellcome to JS Land...");
+//   alert ("Happy Codding");
+
+// //Question no 5//
+  // alert ("Hello... I can run JS through my web browser's console");
+
+//Question no 6//
+// var name = ("Jhon Don");
+//var age = ("15 year old");
+ //var studentdata  = ("Mobile Application developer ");
+ //alert (name);
+ // alert (age);
+// alert (studentdata);
+
+
+// //Queston no 7//
+// var a = ("PIZZA");
+// alert (a+"\nPIZZ\nnPI\nnP");
+
+ 
+// //Question no 8//
+//   var a =  (" My Email address is hammadbashir@gmail.conm");
+//   alert (a);
+
+// //Question no 9//
+// var a = ("Yeah!I can write HTML content through JavaScript")
+// document.write(a);
+
+//Question no 10//
+// alert ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+//Question no 11//
+// var age = ("I am 15 year old");
+// alert (age);
+ 
+// //Question no 12//
+// var a = ("You have visit this side 14 times");
+// alert(a);
+
+
+// // //Question no 13//
+//  var birth = (" My birth year 2003 Date 29 January");
+// document.write(birth)
+//Quesion no 14//
+// document.write(name=+"ordered"+quantity+product+"from shopping center")
+//Question no 15//
+// document.write(5+3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
